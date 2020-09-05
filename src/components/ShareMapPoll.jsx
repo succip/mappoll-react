cdd5a80fd0485d214c705cad83480a47f88a3a55
Paolo Succi <succi@SucciMacbookPro.local>
2020-09-05 09:19:01 -0700

    Added copy to clipboard button to ShareMapPoll

--- src/components/ShareMapPoll.jsx
+++ src/components/ShareMapPoll.jsx
@@ -3,16 +3,32 @@ import React, { Component } from "react";
 class ShareMapPoll extends Component {
   state = {};
   mapPollId = this.props.match.params.mapPollId;
+
+  copyToClipboard = () => {
+    const el = this.h3;
+    el.select();
+    document.execCommand("copy");
+  };
+
   render() {
     return (
-      <>
-        <h4>Your MapPoll is ready!</h4>
-        <p>It can be found here: </p>
-        <h3>
-          <a href={`/mappoll/` + this.mapPollId}>mappoll.com/{this.mapPollId} </a>
-        </h3>
-        <p>Share this link with your friends to start polling.</p>
-      </>
+      <div className="container">
+        <div className="row justify-content-center">
+          <h4>Your MapPoll is ready!</h4>
+        </div>
+        <div className="row justify-content-center">
+          <p>It can be found here: </p>
+        </div>
+        <div className="row justify-content-center">
+          <h3>
+            <a href={`/mappoll/` + this.mapPollId}>mappoll.com/{this.mapPollId} </a>
+          </h3>
+          <button className="ml-3 btn btn-outline-primary btn-sm" onClick={() => this.copyToClipboard}>
+            Copy to Clipboard
+          </button>
+        </div>
+        <h4 className="mt-3 text-secondary">Share this link with your friends to start polling!</h4>
+      </div>
     );
   }
 }
