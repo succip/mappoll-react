2b15377003b4ec51b8aa5e5ab0b577a6addbe818
Paolo Succi <succi@SucciMacbookPro.local>
2020-09-06 13:59:23 -0700

    Updated copylink to check for current domain

--- src/components/ShareMapPoll.jsx
+++ src/components/ShareMapPoll.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { BiCopy } from "react-icons/bi";
 
 class ShareMapPoll extends Component {
   state = { mapPollId: this.props.match.params.mapPollId };
 
+  componentDidMount() {}
+
   copyToClipboard = () => {
-    const shareLink = `localhost:3000/mappoll/${this.state.mapPollId}`
-    const el = document.createElement('textarea');
-    document.body.appendChild(el);
-    el.value = shareLink;
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
+    const url = window.location.href;
+    const urlDomain = url.replace("http://", "").replace("https://", "").split(/[/?#]/)[0];
+    const shareLink = urlDomain + this.props.location.pathname;
+    const dummyElement = document.createElement("textarea");
+    document.body.appendChild(dummyElement);
+    dummyElement.value = shareLink;
+    dummyElement.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummyElement);
   };
 
   render() {
@@ -20,14 +25,11 @@ class ShareMapPoll extends Component {
           <h4>Your MapPoll is ready!</h4>
         </div>
         <div className="row justify-content-center">
-          <p>It can be found here: </p>
-        </div>
-        <div className="row justify-content-center">
           <h3>
             <a href={`/mappoll/` + this.state.mapPollId}>mappoll.com/{this.state.mapPollId} </a>
           </h3>
           <button className="ml-3 btn btn-outline-primary btn-sm" onClick={this.copyToClipboard}>
-            Copy Link
+            Copy <BiCopy />
           </button>
         </div>
         <h4 className="mt-3 text-secondary">Share this link with your friends to start polling!</h4>
